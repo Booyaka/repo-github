71832309b7abe51fdfe5ec55088f94b4f1e98ee6
Booyaka <79873490+Booyaka@users.noreply.github.com>
2021-03-16 18:20:30 +0300

    Add files via upload

---
+++ python_ht_2_1.py
@@ -0,0 +1,10 @@
+"""
+1. Выяснить тип результата выражений:
+"""
+
+example1 = 15 * 3
+example2 = 15 / 3
+example3 = 15 // 2
+example4 = 15 ** 2
+print(f'Type of examples: ex1: {example1} {type(example1)}, ex2: {example2} {type(example2)}, \n'
+      f'ex3: {example3} {type(example3)} ex3: {example4} {type(example4)}')
